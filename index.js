85180140251144ad86b06ec86fab7e2d722d1e3c
samiullahqureshi1 <samiullahqureshi669@gmail.com>
2024-10-06 08:15:40 -0700

    done

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 import dotenv from "dotenv";
 import express from "express";
-import favicon from "serve-favicon";
-import path from "path"; // Import path to resolve the file path
-import { fileURLToPath } from "url"; // Import for getting the current file URL
 
 import { dbConnection } from "./db_connection.js";
 
@@ -19,11 +16,7 @@ dbConnection();
 app.use(express.json());
 
 // Get the current file's directory
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-// Serve favicon
-app.use(favicon(path.join(__dirname, 'favicon.ico')));
 
 // Basic route
 app.get('/', (req, res) => {
