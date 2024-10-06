f1a896915cbeb766626e0ac758c69fa5db244894
samiullahqureshi1 <samiullahqureshi669@gmail.com>
2024-10-06 08:07:10 -0700

    done

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import dotenv from "dotenv";
 import express from "express";
+import favicon from "serve-favicon";
 import { dbConnection } from "./db_connection.js";
 const app = express();
 dotenv.config();
 dbConnection();
 
 app.use(express.json());
-
-
+app.use(favicon(path.join(__dirname, 'favicon.ico'))); 
+app.get('/', (req, res) => {
+    res.send('Welcome to the API!'); // You can customize this message
+});
 
 
 
